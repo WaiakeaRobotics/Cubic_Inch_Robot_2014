e2717b936786b6af22d66afd382710d66e264336
Chester <hilo90mhz@gmail.com>
2014-07-13 15:48:20 -1000

    a

--- Cubic_Inch_Robot_2014.ino
+++ Cubic_Inch_Robot_2014.ino
@@ -44,11 +44,14 @@ versus going down all the way to 10 if it was not aware of the loopover effect o
 
 // I2Cdev and MPU6050 must be installed as libraries, or else the .cpp/.h files
 // for both classes must be in the include path of your project
-#include "LOCAL_I2Cdev.h"
+//#include "LOCAL_I2Cdev.h"
 
-#include "LOCAL_MPU6050_6Axis_MotionApps20.h"
+//#include "LOCAL_MPU6050_6Axis_MotionApps20.h"
 //#include "MPU6050.h" // not necessary if using MotionApps include file
 
+#include "I2Cdev.h"
+
+#include "MPU6050_6Axis_MotionApps20.h"
 
 // Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
 // is used in I2Cdev.h
